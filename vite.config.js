afc405f81214550afb0f8c44fe146013d858ac1c
Criste Catalin (CljP/MOE-P) <crc3clj@bosch.com>
2026-05-12 08:30:35 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/elpc/',
+  base: command === 'serve' ? '/' : '/CRC3CLJ/elpc/',
   plugins: [react()],
   server: {
     open: true
