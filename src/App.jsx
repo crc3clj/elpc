b35aa2e192bd1200b367e482118be87c74d4d0a3
Criste Catalin (CljP/MOE-P) <crc3clj@bosch.com>
2026-05-12 08:38:59 +0300

    Fix routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Elpc from "./ELPC";
 import Editor from "./Editor";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Elpc />} />
         <Route path="/editor" element={<Editor />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/CRC3CLJ/elpc/',
+export default defineConfig({
+  base: './',
   plugins: [react()],
-  server: {
-    open: true
-  }
-}))
+})
